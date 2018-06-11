3c3116c65f14f424006bd2920e31146df5129889
Ryan Ghaida <ryaboug@gmail.com>
2018-06-11 17:39:04 -0400

    done

--- src/index.js
+++ src/index.js
@@ -13,26 +13,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const taskPriority = document.getElementById("new-task-priority");
   newTaskForm.addEventListener('submit', createNewTask)
 
-  const itemArray = [];
+  list.addEventListener('click', deleteList);
+
+  function deleteList(e){
+    if(e.target.className === 'delete-list') {
+      let name = e.target.dataset.title
+      let item = document.getElementById(name)
+      list.removeChild(item)
+      let optionToDelete = document.querySelector(`option[data-name="${name}"]`);
+      optionToDelete.remove()
+      // a[href^="#"]
+      // listDropDown.innerHTML = listDropDown.innerHTML - `<option data-name='${name}' value='option-${name}'>`
+      // listDropDown.innerHtml -= `<option data-name='${name}' value='option-${name}'>`
+      // listDropDown.removeChild(option)
+    }
+
+    else if (e.target.className === 'delete-task'){
+      let description = e.target.dataset.description
+      // debugger
+      let del = document.querySelector(`li[data-description="${description}"]`)
+      del.remove()
+    }
+  }
+
+
 
   function createNewTask(e){
     e.preventDefault()
       // Find data-set name
-      let dataSetNAme = document.get
+      // let dataSetNAme = document.get
 
-     const selectedList = document.querySelector('option').value
-    document.querySelector('[value="option-apples"]').value
-
-    const split = selectedList.split("-")[1]
-    const ul = document.getElementById(`ul-container-${split}`)
-    ul.innerHTML += `<li > Description: ${taskDescription.value} Priority: ${taskPriority.value} </li>`
+     let dropDown = document.getElementById('parent-list')
+     let dropDownItem = dropDown.options[dropDown.selectedIndex].text
+    // document.querySelector('[value="option-apples"]').value
+    // debugger
+    const ul = document.getElementById(`ul-container-${dropDownItem}`)
+    ul.innerHTML += `<li data-description='${taskDescription.value}'> Description: ${taskDescription.value}<button data-description='${taskDescription.value}' class="delete-task"> X </button>Priority: ${taskPriority.value}</li>`
   }
 
   function createNewList(e) {
     e.preventDefault()
 
-    itemArray.push({name: listInput.value, description: null, priority: null})
-    list.innerHTML += `<div>
+    // itemArray.push({name: listInput.value, description: null, priority: null})
+    list.innerHTML += `<div id='${listInput.value}'>
                       <h2> ${listInput.value}
                         <button data-title='${listInput.value}' class="delete-list">
                           X
